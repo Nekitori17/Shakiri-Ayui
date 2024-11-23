dcd133a7916d2f481aeee9388cf52fcba4b495f2
Nekitori17 <cuongnguyen286641@gmail.com>
2024-11-23 19:54:32 +0700

    chore(0: out of range pages

--- src/commands/music/queue.ts
+++ src/commands/music/queue.ts
@@ -28,74 +28,79 @@ const command: CommandInterface = {
       const queuePartition: Track[][] = [];
       let currentPage = 0;
       const tracksArray = queue?.tracks.toArray() || [];
-      const maxPage = Math.floor(tracksArray.length / 10) + 1;
+      const maxPage = Math.ceil(tracksArray.length / 10);
       for (let i = 0; i < maxPage; i++) {
         queuePartition.push(tracksArray.slice(i * 10, (i + 1) * 10));
       }
 
-      function createEmbedForPage(page: number): EmbedBuilder {
-        return new EmbedBuilder()
-          .setAuthor({
-            name: interaction.user.displayName,
-            iconURL: interaction.user.displayAvatarURL(),
-          })
-          .setTitle("> 🎶 Queue")
-          .setDescription(
-            queuePartition[page]
-              .map(
-                (track, index) =>
-                  `* ${page * 10 + index + 1}. ${track.title} - ${track.author}`
+      function createReply(page: number) {
+        const buttonsPage = new ActionRowBuilder<ButtonBuilder>({
+          components: [
+            new ButtonBuilder()
+              .setCustomId("musicQueuePagePrevious")
+              .setEmoji("⬅️")
+              .setStyle(ButtonStyle.Primary),
+            new ButtonBuilder()
+              .setCustomId("musicQueuePageCurrent")
+              .setLabel(`${page + 1}/${maxPage}`)
+              .setStyle(ButtonStyle.Secondary),
+            new ButtonBuilder()
+              .setCustomId("musicQueuePageNext")
+              .setEmoji("➡️")
+              .setStyle(ButtonStyle.Primary),
+          ],
+        });
+        return {
+          embeds: [
+            new EmbedBuilder()
+              .setAuthor({
+                name: interaction.user.displayName,
+                iconURL: interaction.user.displayAvatarURL(),
+              })
+              .setTitle("> 🎶 Queue")
+              .setDescription(
+                queuePartition[page]
+                  .map(
+                    (track, index) =>
+                      `* ${page * 10 + index + 1}. ${track.title} - ${
+                        track.author
+                      }`
+                  )
+                  .join("\n")
               )
-              .join("\n")
-          )
-          .setFooter({
-            text: interaction.guild?.name!,
-          })
-          .setTimestamp()
-          .setColor("#00ffc8");
+              .setFooter({
+                text: interaction.guild?.name!,
+              })
+              .setTimestamp()
+              .setColor("#00ffc8"),
+          ],
+          components: [buttonsPage],
+        };
       }
 
-      const buttonsPage = new ActionRowBuilder<ButtonBuilder>({
-        components: [
-          new ButtonBuilder()
-            .setCustomId("musicQueuePagePrevious")
-            .setEmoji("⬅️")
-            .setStyle(ButtonStyle.Primary),
-          new ButtonBuilder()
-            .setCustomId("musicQueuePageNext")
-            .setEmoji("➡️")
-            .setStyle(ButtonStyle.Primary),
-        ],
-      });
+      const reply = await interaction.editReply(createReply(currentPage));
 
-      const reply = await interaction.editReply({
-        embeds: [createEmbedForPage(currentPage)],
-        components: [buttonsPage],
-      });
-
-      const filter = (i: any) => i.user.id === interaction.user.id;
       const collector = reply.createMessageComponentCollector({
         componentType: ComponentType.Button,
-        filter: filter,
+        filter: (i) => i.user.id === interaction.user.id,
         time: 60_000,
       });
 
       collector.on("collect", async (inter) => {
         if (inter.customId === "musicQueuePagePrevious") {
-          if (currentPage === 0) return inter.deferUpdate();;
+          if (currentPage === 0) return inter.deferUpdate();
           currentPage -= 1;
-          reply.edit({
-            embeds: [createEmbedForPage(currentPage)],
-          });
+          reply.edit(createReply(currentPage));
           inter.deferUpdate();
         }
 
+        if (inter.customId == "musicQueuePageCurrent")
+          return inter.deferUpdate();
+
         if (inter.customId === "musicQueuePageNext") {
-          if (currentPage === maxPage - 1) return inter.deferUpdate();;
+          if (currentPage === maxPage - 1) return inter.deferUpdate();
           currentPage += 1;
-          reply.edit({
-            embeds: [createEmbedForPage(currentPage)],
-          });
+          reply.edit(createReply(currentPage));
           inter.deferUpdate();
         }
       });
