b88b0454fa241ed5bd8421708c06f323d4bc76e7
Nekitori17 <cuongnguyen286641@gmail.com>
2024-11-23 19:56:25 +0700

    build(): add script run for host not support typescript instantly

--- scripts/run.js
+++ scripts/run.js
@@ -5,11 +5,11 @@ const args = ["start"];
 const process = spawn(command, args, { shell: true });
 
 process.stdout.on("data", (data) => {
-    console.log(data);
+    console.log(data.toString());
 });
 
 process.stderr.on("data", (data) => {
-    console.error(data);
+    console.error(data.toString());
 });
 
 process.on("close", (code) => {
