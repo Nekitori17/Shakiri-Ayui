ca1e84430998ba8128040040b1144a019a8f8020
Nekitori17 <cuongnguyen286641@gmail.com>
2025-01-21 21:50:51 +0700

    chore(): change category case style

---
+++ src/commands/userSettings/translate-set.ts
@@ -0,0 +1,58 @@
+import { ApplicationCommandOptionType } from "discord.js";
+import { sendError } from "../../utils/sendError";
+import UserSettings from "../../models/UserSettings";
+import { translateLanguages } from "../../constants/translateLanguages";
+import { CommandInterface } from "../../types/InteractionInterfaces";
+
+const command: CommandInterface = {
+  async execute(interaction, client) {
+    await interaction.deferReply();
+    const language = interaction.options.get("lang")
+      ?.value as keyof typeof translateLanguages;
+
+    try {
+      if (!Object.keys(translateLanguages).includes(language))
+        throw {
+          name: "Language Not Found",
+          message: "Please enter a valid language (Ex: en, vi, ja,...)",
+        };
+
+      const data = await UserSettings.findOne({
+        userId: interaction.user.id,
+      });
+
+      if (data) {
+        data.messageTranslateLang = language;
+
+        await data.save();
+      } else {
+        const newData = new UserSettings({
+          userId: interaction.user.id,
+          messageTranslateLang: language,
+        });
+
+        await newData.save();
+      }
+
+      interaction.editReply(
+        `> <:update:1309527728999104622> You have set your translate language to \`${translateLanguages[language]}\``
+      );
+    } catch (error) {
+      sendError(interaction, error);
+    }
+  },
+  name: "translate-set",
+  description: "Set translate language",
+  deleted: false,
+  canUseInDm: true,
+  options: [
+    {
+      name: "lang",
+      description: "Language to translate (ex: en, vi, ja,...)",
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    },
+  ],
+};
+
+export default command;
--- src/models/UserSettings.ts
+++ src/models/UserSettings.ts
@@ -4,10 +4,13 @@ const UserSettings = new Schema({
   userId: { type: String, required: true },
   messageTranslateLang: String,
   temporaryVoiceChannel: {
-    channelName: String,
-    limitUser: Number,
-    blockedUsers: [String],
-  }
+    type: new Schema({
+      channelName: String,
+      limitUser: Number,
+      blockedUsers: [String],
+    }),
+    default: {},
+  },
 });
 
 export default model("UserSettings", UserSettings);
