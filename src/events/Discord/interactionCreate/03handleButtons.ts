b535f5e56d77de6dcb776a5687fb74d386c4be54
Nekitori17 <cuongnguyen286641@gmail.com>
2025-04-13 16:48:23 +0700

    feat: handle button interaction

---
+++ src/events/Discord/interactionCreate/03handleButtons.ts
@@ -0,0 +1,89 @@
+import config from "../../../config";
+import path from "path";
+import {
+  ButtonInteraction,
+  GuildMember,
+  PermissionsBitField,
+} from "discord.js";
+import sendError from "../../../helpers/sendError";
+import getLocalById from "../../../helpers/getLocalById";
+import { DiscordEventInterface } from "../../../types/EventInterfaces";
+import { ButtonInterface } from "../../../types/InteractionInterfaces";
+
+const event: DiscordEventInterface = async (
+  client,
+  interaction: ButtonInteraction
+) => {
+  if (!interaction.isButton()) return;
+  if (!interaction.customId.startsWith("$")) return;
+
+  try {
+    const [category, customId] = interaction.customId.split("_");
+    
+    const buttonObject = getLocalById<ButtonInterface>(
+      path.join(__dirname, "../../../menus/buttons"),
+      category.replace("$", ""),
+      customId
+    );
+
+    if (!buttonObject) return;
+
+    if (buttonObject.voiceChannel) {
+      if (!(interaction.member as GuildMember).voice.channel)
+        throw {
+          name: "NoVoiceChannel",
+          message: "To use this command, you must be in a voice channel",
+        };
+    }
+
+    buttonObject.botPermissions?.push(...config.defaultPermissions);
+    if (buttonObject.botPermissions?.length) {
+      for (const permission of buttonObject.botPermissions) {
+        if (
+          !(
+            interaction.guild?.members.me?.permissions as PermissionsBitField
+          ).has(permission)
+        ) {
+          throw {
+            name: "MissingPermissions",
+            message: `I'am missing the \`${new PermissionsBitField(permission)
+              .toArray()
+              .join(", ")}\` permission to use this command.`,
+          };
+        }
+      }
+    }
+
+    if (buttonObject.permissionsRequired?.length) {
+      for (const permission of buttonObject.permissionsRequired) {
+        if (
+          !(interaction.member?.permissions as PermissionsBitField).has(
+            permission
+          )
+        )
+          throw {
+            name: "MissingPermissions",
+            message: `You are missing the \`${new PermissionsBitField(
+              permission
+            )
+              .toArray()
+              .join(", ")}\` permission to use this command.`,
+          };
+      }
+    }
+
+    buttonObject.execute(interaction, client);
+  } catch (error: { name: string; message: string } | any) {
+    if (error instanceof Error) {
+      console.log(
+        `\x1b[31m\x1b[1m|> ${error.name} (Command Interaction)\x1b[0m`
+      );
+      console.log(`\x1b[32m${error.message}\x1b[0m`);
+      console.log(error);
+    }
+
+    sendError(interaction, error, true);
+  }
+};
+
+export default event;
