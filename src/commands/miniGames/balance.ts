b74a2dcbf9e58ed933625890ff57032b9678d78b
Nekitori17 <cuongnguyen286641@gmail.com>
2025-05-20 21:21:27 +0700

    fix(): Prevents bot users in balance command
    Adds a check to ensure that the user target isn't null
    before attempting to fetch the user.
    
    This prevents the command from throwing an error when no user is specified.

--- src/commands/miniGames/balance.ts
+++ src/commands/miniGames/balance.ts
@@ -9,7 +9,7 @@ const command: CommandInterface = {
     const userTarget = interaction.options.get("user")?.value as string;
 
     try {
-      if ((await client.users.fetch(userTarget)).bot)
+      if (userTarget && (await client.users.fetch(userTarget)).bot)
         throw {
           name: "BotUser",
           message: "Bro think they's a humman 💀🙏",
